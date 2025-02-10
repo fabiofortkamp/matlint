31b7940cfd40b1e9add1dfc18da98adf2cd3c89c
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-10 14:33:20 +0100

    Initial ruby version

---
+++ lib/matlint.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "matlint/version"
+
+module Matlint
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/matlint_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Matlint do
+  it "has a version number" do
+    expect(Matlint::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
