ca9fc29f02d62de53901190693b84c1600c8c243
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-17 21:13:02 +0100

    Create basic model classes

--- lib/matlint.rb
+++ lib/matlint.rb
@@ -5,4 +5,20 @@ require_relative "matlint/version"
 module Matlint
   class Error < StandardError; end
   # Your code goes here...
+
+  class MatlabFunction
+    attr_reader :name, :inputs, :outputs, :body
+
+    def to_s
+      "MATLAB Function:\nName:#{@name}\nInputs:#{@outputs}\nOutputs:#{@outputs}"
+    end
+  end
+
+  class MatlabCodeBlock
+    attr_reader :header, :expressions
+  end
+
+  class MatlabExpression
+    attr_reader :content
+  end
 end
